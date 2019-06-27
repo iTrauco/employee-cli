c2801d149f3fd4d4882d6aa376694ba516aca37f
Christopher Trauco <Chris@Trau.co>
2019-06-27 14:18:19 -0400

    CLI EU questions functional

--- commands.js
+++ commands.js
@@ -1,21 +1,50 @@
 const program = require('commander');
+const { prompt } = require('inquirer');
 const {
     addEmployee, 
     findEmployee
 } = require('./index');
-
+//
+//////==================================================================
+//////==================================================================
+// End User Questions
+const questions = [
+    {
+        type: 'input',
+        name: 'firstname',
+        message: 'Employee First Name'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'Employee Last Name'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'Employee Work Number'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Employee Email'
+    }
+];
+//
+//////==================================================================
+//////==================================================================
+// 
 program
     .version('1.0.0')
     .description('Employee Management System')
 
 program
-  .command('add <firstname> <lastname> <phone> <email>')
-  .alias('a')
-  .description('Add a employee')
-  .action((firstname, lastname, phone, email) => {
-    addEmployee({firstname, lastname, phone, email});
-  });
-
+    .command('add')
+    .alias('a')
+    .description('Add a customer')
+    .action(() => {
+        prompt(questions).then(answers => addEmployee(answers));
+});
 
 program
     .command('find <name>')
